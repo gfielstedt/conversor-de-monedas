948dd00a30f787f11bbf7c0bc30ed7f13330d78a
Ginessa <gfielstedt@gmail.com>
2024-01-31 00:45:36 -0300

    getIndicadorFunction

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+/*variables*/
+const apiURL = "https://mindicador.cl/api/";
+
+const montoInput = document.querySelector("#montoInput");
+const btnConvertir = document.querySelector("#btnConvertir");
+const montoConvertido = document.querySelector("#montoConvertido");
+const tipoDeMoneda = document.querySelector("#monedasParaConvertir");
+
+let data = {};
+let selectedCurrencyData = {}; // Almacenar los datos de la moneda seleccionada
+let myChart = null; // Inicializamos myChart a null
+
+/*funciones*/
+
+async function getIndicador() {
+  try {
+    const res = await fetch(apiURL);
+    console.log(apiURL);
+    data = await res.json();
+    btnConvertir.disabled = false;
+  } catch (error) {
+    alert("Error al conseguir los datos de conversión: " + error.message);
+  }
+}
